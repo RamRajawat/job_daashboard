32548766e5b104d5274a79136bacb253402276cd
RamRajawat <ramrajawat2002@gmail.com>
2025-06-13 15:58:55 +0530

    second

---
+++ backend/dist/job/job.entity.d.ts
@@ -0,0 +1,13 @@
+export declare class Job {
+    id: number;
+    title: string;
+    companyName: string;
+    location: string;
+    jobType: string;
+    salaryMin: number;
+    salaryMax: number;
+    deadline: string;
+    description: string;
+    requirements: string;
+    responsibilities: string;
+}
